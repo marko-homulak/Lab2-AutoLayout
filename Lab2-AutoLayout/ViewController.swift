8d2002fcef71da43fe78e2ca588b5484d43f71dd
Marko Khomulyak <markotanki2016@gmail.com>
2023-11-09 19:38:42 +0200

    First commit

---
+++ Lab2-AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2-AutoLayout
+//
+//  Created by Marko Khomulyak on 09.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
